00f3f1201eb56e8dbb5a90ce033e35f6676f6e29
Rayana-Padmini <prayana65@gmail.com>
2017-09-12 14:18:16 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+		StudentGroup sg = new StudentGroup(3);
+		//sg.add({7, "xyz", "1997-08-07", 58.6}, 0);
+	}
+
+}
